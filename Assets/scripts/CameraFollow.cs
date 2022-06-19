c5ba8e1ad9924c410da0a7df01286bd359789c31
Diego Enrique Hernández Cholotío <104159636+unhumanosinmas@users.noreply.github.com>
2022-06-18 22:17:30 -0600

    Camara Update, Main Escene
    Se mejoro la cámara para que siga al jugador, y se inicio la implementación de la escena real del juego

---
+++ Assets/scripts/CameraFollow.cs
@@ -0,0 +1,33 @@
+
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform target;
+    public Vector3 offset;
+
+    [Range(1,10)]
+    public float smoothFactor;
+
+
+    public Vector3 Dead;
+
+
+    private void FixedUpdate()
+    {
+        if (target == null)
+        {
+            transform.position = Dead;
+        }
+        else { Follow(); }
+    }
+
+    void Follow() {
+        Vector3 targetPosition = target.position + offset;
+
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, targetPosition, smoothFactor*Time.fixedDeltaTime);
+
+        transform.position = smoothPosition;
+    }
+}
